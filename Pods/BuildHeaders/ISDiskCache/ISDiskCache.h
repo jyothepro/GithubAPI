723cbf82673e59989a0f60bfc6f9e20420080b89
Jyothidhar Pulakunta <cooljyo@gmail.com>
2013-10-05 14:36:44 -0700

    updating pod file contents

---
+++ Pods/BuildHeaders/ISDiskCache/ISDiskCache.h
@@ -0,0 +1,1 @@
+../../ISDiskCache/ISDiskCache/ISDiskCache.h
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -48,6 +48,12 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 0
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 0
 
+// ISDiskCache
+#define COCOAPODS_POD_AVAILABLE_ISDiskCache
+#define COCOAPODS_VERSION_MAJOR_ISDiskCache 0
+#define COCOAPODS_VERSION_MINOR_ISDiskCache 1
+#define COCOAPODS_VERSION_PATCH_ISDiskCache 0
+
 // JSONKit
 #define COCOAPODS_POD_AVAILABLE_JSONKit
 // This library does not follow semantic-versioning,
@@ -61,39 +67,9 @@
 #define COCOAPODS_VERSION_MINOR_SDWebImage 4
 #define COCOAPODS_VERSION_PATCH_SDWebImage 0
 
-// SDWebImage/MapKit
-#define COCOAPODS_POD_AVAILABLE_SDWebImage_MapKit
-#define COCOAPODS_VERSION_MAJOR_SDWebImage_MapKit 3
-#define COCOAPODS_VERSION_MINOR_SDWebImage_MapKit 4
-#define COCOAPODS_VERSION_PATCH_SDWebImage_MapKit 0
-
-// libwebp
-#define COCOAPODS_POD_AVAILABLE_libwebp
-#define COCOAPODS_VERSION_MAJOR_libwebp 0
-#define COCOAPODS_VERSION_MINOR_libwebp 3
-#define COCOAPODS_VERSION_PATCH_libwebp 0
-
-// libwebp/dec
-#define COCOAPODS_POD_AVAILABLE_libwebp_dec
-#define COCOAPODS_VERSION_MAJOR_libwebp_dec 0
-#define COCOAPODS_VERSION_MINOR_libwebp_dec 3
-#define COCOAPODS_VERSION_PATCH_libwebp_dec 0
-
-// libwebp/dsp
-#define COCOAPODS_POD_AVAILABLE_libwebp_dsp
-#define COCOAPODS_VERSION_MAJOR_libwebp_dsp 0
-#define COCOAPODS_VERSION_MINOR_libwebp_dsp 3
-#define COCOAPODS_VERSION_PATCH_libwebp_dsp 0
-
-// libwebp/utils
-#define COCOAPODS_POD_AVAILABLE_libwebp_utils
-#define COCOAPODS_VERSION_MAJOR_libwebp_utils 0
-#define COCOAPODS_VERSION_MINOR_libwebp_utils 3
-#define COCOAPODS_VERSION_PATCH_libwebp_utils 0
-
-// libwebp/webp
-#define COCOAPODS_POD_AVAILABLE_libwebp_webp
-#define COCOAPODS_VERSION_MAJOR_libwebp_webp 0
-#define COCOAPODS_VERSION_MINOR_libwebp_webp 3
-#define COCOAPODS_VERSION_PATCH_libwebp_webp 0
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 4
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 0
 
